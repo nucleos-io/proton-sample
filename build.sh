cbf281f589785eff73bdca4e8d5a41db624df06c
Carlos Marcano <cj@hustling.me>
2017-03-31 16:59:28 -0400

    Rename image name

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ if [ ! -f .yarn-cache.tgz ]; then
   tar cvzf .yarn-cache.tgz --files-from /dev/null
 fi
 
-docker build . -t bizup
+docker build . -t proton-sample
 
-docker run --rm --entrypoint cat bizup:latest /tmp/yarn.lock > /tmp/yarn.lock
+docker run --rm --entrypoint cat proton-sample:latest /tmp/yarn.lock > /tmp/yarn.lock
 
 if ! diff -q yarn.lock /tmp/yarn.lock > /dev/null  2>&1; then
   echo "We have a new yarn.lock"
